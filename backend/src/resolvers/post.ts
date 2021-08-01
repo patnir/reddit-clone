2a6eb929b6826bc2688c3c8aa44c8b8e6413ba35
Rahul Patni <rahul20patni@gmail.com>
2021-08-01 16:31:20 -0400

    create and get post

--- backend/src/resolvers/post.ts
+++ backend/src/resolvers/post.ts
@@ -1,5 +1,5 @@
 import { Post } from "./../entities/Post";
-import { Ctx, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
 import { MyContext } from "src/types";
 
 @Resolver()
@@ -7,5 +7,23 @@ export class PostResolver {
   @Query(() => [Post])
   posts(@Ctx() { em }: MyContext): Promise<Post[]> {
     return em.find(Post, {});
+  }
+
+  @Query(() => Post)
+  post(
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    return em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("text", () => String) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const post = em.create(Post, { title });
+    await em.persistAndFlush(post);
+    return post;
   }
 }
