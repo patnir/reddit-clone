2e9d6bd2164af921ba4574902f5f839bcc42e09b
Rahul Patni <rahul20patni@gmail.com>
2021-08-01 12:19:25 -0400

    everything upto creating the post table

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello World!")
+import { Post } from "./entities/Post";
+import { MikroORM } from "@mikro-orm/core";
+import { __is_prod__ } from "./constants";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: [Post],
+    user: "test",
+    dbName: "redditclone",
+    debug: !__is_prod__,
+    type: "postgresql",
+  });
+
+  const post = orm.em.create(Post, { title: "my first post" });
+  await orm.em.persistAndFlush(post);
+};
+
+console.log("Hello World! This is the backend");
+main().catch((err) => {
+  console.log("ERROR MAIN()");
+  console.log(err);
+});
