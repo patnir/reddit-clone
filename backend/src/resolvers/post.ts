d919cfe0c5329b01742d2c97b232f6929cc76f60
Rahul Patni <rahul20patni@gmail.com>
2021-08-01 16:44:07 -0400

    post crud complete

--- backend/src/resolvers/post.ts
+++ backend/src/resolvers/post.ts
@@ -9,7 +9,7 @@ export class PostResolver {
     return em.find(Post, {});
   }
 
-  @Query(() => Post)
+  @Query(() => Post, { nullable: true })
   post(
     @Arg("id", () => Int) id: number,
     @Ctx() { em }: MyContext
@@ -25,5 +25,33 @@ export class PostResolver {
     const post = em.create(Post, { title });
     await em.persistAndFlush(post);
     return post;
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg("id", () => Int) id: number,
+    @Arg("text", () => String) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { id });
+    if (post) {
+      post.title = title;
+      await em.persistAndFlush(post);
+      return post;
+    }
+    return null;
+  }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg("id", () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<boolean> {
+    const post = await em.findOne(Post, { id });
+    if (post) {
+      await em.removeAndFlush(post);
+      return true;
+    }
+    return false;
   }
 }
